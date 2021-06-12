01e41158d558d472f780b3a3f342992268d028f4
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-12 23:04:53 +0530

    Added the part time functionality

--- EmployeeAttendance.java
+++ EmployeeAttendance.java
@@ -1,14 +1,24 @@
 class EmployeeAttendance{
 	static final int IsPresent = 1;
+	static final int IsPartTime = 2;
 	static final int WagePerHour = 20;
-	static final int NumOfHours = 8;
+
 	public static void main(String args[]){
 		System.out.println("Welcome to wage computation problem");	
-		double check = Math.floor(Math.random()* 10) % 2;
+		double check = Math.floor(Math.random()* 10) % 3;
 		if(check == IsPresent){
+			final int NumOfHours = 8;
 			System.out.println("Employee is present");
 			int empWage = WagePerHour*NumOfHours;
 			System.out.println("The wage of the employee is:"+empWage);
+		}
+		else if(check == IsPartTime){
+			System.out.println("Employee is present part time");
+			final int NumOfHours = 4;
+			int empWage = WagePerHour*NumOfHours;
+			System.out.println("The wage of the employee is:"+empWage);
+
+
 		}
 		else{
 			System.out.println("Employee is absent");
