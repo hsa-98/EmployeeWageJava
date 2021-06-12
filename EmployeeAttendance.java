dcd981163716db8b5d346a50c281f874e600e0d7
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-12 23:37:56 +0530

    Calculated the wage for 20 working days

--- EmployeeAttendance.java
+++ EmployeeAttendance.java
@@ -4,26 +4,37 @@ class EmployeeAttendance{
 	static final int WagePerHour = 20;
 
 	public static void main(String args[]){
-		System.out.println("Welcome to wage computation problem");	
-		double check = Math.floor(Math.random()* 10) % 3;
-		switch((int)check){
-			case IsPresent:
-				int NumOfHours = 8;
-				System.out.println("Employee is present");
-				int empWage = WagePerHour*NumOfHours;
-				System.out.println("The wage of the employee is:"+empWage);
-				break;
+		System.out.println("Welcome to wage computation problem");
+		int totalDays = 0;
+		int totalSalary = 0;
+		while(totalDays <=20) {
+			double check = Math.floor(Math.random() * 10) % 3;
+			switch ((int) check) {
+				case IsPresent:
+					int NumOfHours = 8;
+					System.out.println("Employee is present");
+					int empWage = WagePerHour * NumOfHours;
+					System.out.println("The wage of the employee is:" + empWage);
+					totalDays++;
+					totalSalary+=empWage;
+					break;
 
-			case IsPartTime:
-				System.out.println("Employee is present part time");
-				NumOfHours = 4;
-				empWage = WagePerHour*NumOfHours;
-				System.out.println("The wage of the employee is:"+empWage);
-				break;
+				case IsPartTime:
+					System.out.println("Employee is present part time");
+					NumOfHours = 4;
+					empWage = WagePerHour * NumOfHours;
+					System.out.println("The wage of the employee is:" + empWage);
+					totalDays++;
+					totalSalary+=empWage;
+					break;
 
-			case 0:
-				System.out.println("Employee is absent");
-				break;
+				case 0:
+					System.out.println("Employee is absent");
+					totalDays++;
+					break;
+			}
 		}
+		System.out.println("The total wage of employee after 20 working days is: " +totalSalary);
 	}
 }
+
