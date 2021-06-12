1d96780ddbd6167f8c4197ef3af674ee9386a238
hsa-98 <harshsanjayagrawal89@gmail.com>
2021-06-12 22:47:40 +0530

    Checks if the employee is present or absent

--- EmployeeAttendance.java
+++ EmployeeAttendance.java
@@ -1,7 +1,13 @@
 class EmployeeAttendance{
+	static final int IsPresent=1;
 	public static void main(String args[]){
 		System.out.println("Welcome to wage computation problem");	
-
-
+		double check = Math.floor(Math.random()* 10) % 2;
+		if(check == IsPresent){
+			System.out.println("Employee is present");
+		}
+		else{
+			System.out.println("Employee is absent");
+		}
 	}
 }
